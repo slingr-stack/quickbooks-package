7f51f6c10a1c41efc8b0e4b6a3d71fee6f92bf90
Angel Perez Personal <pasaperez@yahoo.com.ar>
2023-11-15 13:45:08 -0300

    remove helpers

--- flowSteps/apiCallQuickbooks/step.js
+++ flowSteps/apiCallQuickbooks/step.js
@@ -32,20 +32,19 @@ step.apiCallQuickbooks = function (inputs) {
 		fullResponse: inputs.fullResponse || false,
 		connectionTimeout: inputs.connectionTimeout || 5000,
 		readTimeout: inputs.readTimeout || 60000,
-		url: {
-			urlValue: inputs.url.urlValue ? inputs.url.urlValue.split(" ")[1] : "",
-			paramsValue: inputs.url.paramsValue || []
+		path: inputs.path || {
+			urlValue: "",
+			paramsValue: []
 		},
-		method: inputs.url.urlValue ? inputs.url.urlValue.split(" ")[0] : ""
+		method: inputs.method || "get",
 	};
 
 	inputsLogic.headers = isObject(inputsLogic.headers) ? inputsLogic.headers : stringToObject(inputsLogic.headers);
 	inputsLogic.params = isObject(inputsLogic.params) ? inputsLogic.params : stringToObject(inputsLogic.params);
 	inputsLogic.body = isObject(inputsLogic.body) ? inputsLogic.body : JSON.parse(inputsLogic.body);
 
-
 	var options = {
-		url: config.get("QUICKBOOKS_API_BASE_URL") + parse(inputsLogic.url.urlValue, inputsLogic.url.paramsValue),
+		path: parse(inputsLogic.path.urlValue, inputsLogic.path.paramsValue),
 		params: inputsLogic.params,
 		headers: inputsLogic.headers,
 		body: inputsLogic.body,
@@ -57,6 +56,10 @@ step.apiCallQuickbooks = function (inputs) {
 		connectionTimeout: inputsLogic.connectionTimeout,
 		readTimeout: inputsLogic.readTimeout
 	}
+
+	options= setApiUri(options);
+	options= setRequestHeaders(options);
+	options= setAuthorization(options);
 
 	switch (inputsLogic.method.toLowerCase()) {
 		case 'get':
@@ -82,33 +85,28 @@ step.apiCallQuickbooks = function (inputs) {
 	return null;
 };
 
-var parse = function (url, pathVariables){
-
+function parse (url, pathVariables){
 	var regex = /{([^}]*)}/g;
-
 	if (!url.match(regex)){
 		return url;
 	}
-
 	if(!pathVariables){
 		sys.logs.error('No path variables have been received and the url contains curly brackets\'{}\'');
 		throw new Error('Error please contact support.');
 	}
-
 	url = url.replace(regex, function(m, i) {
 		return pathVariables[i] ? pathVariables[i] : m;
 	})
-
 	return url;
 }
 
-var isObject = function (obj) {
+function isObject (obj) {
 	return !!obj && stringType(obj) === '[object Object]'
-};
+}
 
 var stringType = Function.prototype.call.bind(Object.prototype.toString);
 
-var stringToObject = function (obj) {
+function stringToObject (obj) {
 	if (!!obj){
 		var keyValue = obj.toString().split(',');
 		var parseObj = {};
@@ -118,4 +116,47 @@ var stringToObject = function (obj) {
 		return parseObj;
 	}
 	return null;
-};
+}
+
+function setApiUri(options) {
+	let QUICKBOOKS_API_BASE_URL = "https://quickbooks.api.intuit.com/v3";
+	let QUICKBOOKS_API_BASE_URL_SANDBOX = "https://sandbox-quickbooks.api.intuit.com/v3";
+	let API_URL = config.get("quickBooksEnvironment") === "PRODUCTION" ? QUICKBOOKS_API_BASE_URL : QUICKBOOKS_API_BASE_URL_SANDBOX;
+	let url = options.path || "";
+	options.url = concatQuery(API_URL + "/company/" + config.get("companyId") + url, "minorVersion", config.get("minorVersion"));
+	sys.logs.debug('[quickbooks] Set url: ' + options.path + "->" + options.url);
+	return options;
+}
+
+function setRequestHeaders(options) {
+	let headers = options.headers || {};
+	headers = mergeJSON(headers, {"Content-Type": "application/json"});
+	headers = mergeJSON(headers, {"Accept": "application/json"});
+
+	options.headers = headers;
+	return options;
+}
+
+function setAuthorization(options) {
+	sys.logs.debug('[quickbooks] Setting header token oauth');
+	let authorization = options.authorization || {};
+	authorization = mergeJSON(authorization, {
+		type: "oauth2",
+		accessToken: config.get("accessToken"),
+		headerPrefix: "Bearer"
+	});
+	options.authorization = authorization;
+	return options;
+}
+
+function mergeJSON (json1, json2) {
+	const result = {};
+	let key;
+	for (key in json1) {
+		if(json1.hasOwnProperty(key)) result[key] = json1[key];
+	}
+	for (key in json2) {
+		if(json2.hasOwnProperty(key)) result[key] = json2[key];
+	}
+	return result;
+}
--- flowSteps/createAccountQuickbooks/step.js
+++ flowSteps/createAccountQuickbooks/step.js
@@ -19,25 +19,67 @@ step.createAccountQuickbooks = function (inputs) {
 		}
 	}
 
+	options= setApiUri(options);
+	options= setRequestHeaders(options);
+	options= setAuthorization(options);
+
 	return httpService.post(options);
 };
 
-var parse = function (url, pathVariables){
-
+function parse (url, pathVariables){
 	var regex = /{([^}]*)}/g;
-
 	if (!url.match(regex)){
 		return url;
 	}
-
 	if(!pathVariables){
 		sys.logs.error('No path variables have been received and the url contains curly brackets\'{}\'');
 		throw new Error('Error please contact support.');
 	}
-
 	url = url.replace(regex, function(m, i) {
 		return pathVariables[i] ? pathVariables[i] : m;
 	})
-
 	return url;
+}
+
+function setApiUri(options) {
+	let QUICKBOOKS_API_BASE_URL = "https://quickbooks.api.intuit.com/v3";
+	let QUICKBOOKS_API_BASE_URL_SANDBOX = "https://sandbox-quickbooks.api.intuit.com/v3";
+	let API_URL = config.get("quickBooksEnvironment") === "PRODUCTION" ? QUICKBOOKS_API_BASE_URL : QUICKBOOKS_API_BASE_URL_SANDBOX;
+	let url = options.path || "";
+	options.url = concatQuery(API_URL + "/company/" + config.get("companyId") + url, "minorVersion", config.get("minorVersion"));
+	sys.logs.debug('[quickbooks] Set url: ' + options.path + "->" + options.url);
+	return options;
+}
+
+function setRequestHeaders(options) {
+	let headers = options.headers || {};
+	headers = mergeJSON(headers, {"Content-Type": "application/json"});
+	headers = mergeJSON(headers, {"Accept": "application/json"});
+
+	options.headers = headers;
+	return options;
+}
+
+function setAuthorization(options) {
+	sys.logs.debug('[quickbooks] Setting header token oauth');
+	let authorization = options.authorization || {};
+	authorization = mergeJSON(authorization, {
+		type: "oauth2",
+		accessToken: config.get("accessToken"),
+		headerPrefix: "Bearer"
+	});
+	options.authorization = authorization;
+	return options;
+}
+
+function mergeJSON (json1, json2) {
+	const result = {};
+	let key;
+	for (key in json1) {
+		if(json1.hasOwnProperty(key)) result[key] = json1[key];
+	}
+	for (key in json2) {
+		if(json2.hasOwnProperty(key)) result[key] = json2[key];
+	}
+	return result;
 }
